82c8d0465d87f5472ab4255cb66dbe9ddb8d2bda
Polina Kozitskaya <jungspund@yandex.ru>
2026-05-02 13:11:24 +0300

    r14

--- E.java
+++ E.java
@@ -19,12 +19,4 @@ public class E implements C {
     public Object pp() {
         return this;
     }
-
-    public Object gg() {
-        return return getClass().getClassLoader();
-    }
-
-    public java.util.List<String> jj() {
-        return new java.util.ArrayList<String>();
-    }
 }
